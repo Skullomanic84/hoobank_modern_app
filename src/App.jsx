d3d3204f4ba7bb4f08479271b3dd08563b8f4b98
Jahmaine Shiloh <55574810+Skullomanic84@users.noreply.github.com>
2022-11-13 21:16:32 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import styles from './styles';
+import { Billing,Business,CardDeal,Clients,CTA,Footer,Navbar,Stats,Testimonial,Hero } from "./components";
 
 
 function App() {
   
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonial />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
